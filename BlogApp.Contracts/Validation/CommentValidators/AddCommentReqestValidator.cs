7defe1d283d580b7f6db4bcdb376879228cb2d8a
Name <email@example.ru>
2023-07-13 12:44:10 +0300

    add comment and tegs validations

---
+++ BlogApp.Contracts/Validation/CommentValidators/AddCommentReqestValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Contracts.Validation.CommentValidators
+{
+    internal class AddCommentReqestValidator
+    {
+    }
+}
