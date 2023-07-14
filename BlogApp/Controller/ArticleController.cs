39fcfe51560180d9b75924dce8e9c9296e795d27
Name <email@example.ru>
2023-07-14 11:51:32 +0300

    Add Logger

--- BlogApp/Controller/ArticleController.cs
+++ BlogApp/Controller/ArticleController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using BlogApp.Contracts.Models.Articles;
+using BlogApp.Contracts.Models.Users;
 using BlogApp.Data.Repositories;
+using BlogApp.Model;
 using BlogApp.Model.DataModel;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,20 +12,22 @@ namespace BlogApp.Controller
     [Route("[ArticleController]")]
     public class ArticleController: ControllerBase
     {
+        public IUserRepository _user;
         public IArticlRepository _articl;
         public IMapper _mapper;
-        public ArticleController(IArticlRepository articl, IMapper mapper)
+        public ArticleController(IArticlRepository articl, IUserRepository user, IMapper mapper)
         {
             _articl = articl;
             _mapper = mapper;
+            _user = user;
         }
         /// <summary>
         /// Метод для получения всех статей
         /// </summary>
-        /// <returns></returns>
+        /// <returns>StatusCode(200, resp)</returns>
         [HttpGet]
         [Route("")]
-        public async Task<IActionResult> GetArticles()
+        public async Task<IActionResult> GetAllArticles()
         {
             var articles = await _articl.GetArticles();
 
@@ -35,5 +39,48 @@ namespace BlogApp.Controller
 
             return StatusCode(200, resp);
         }
+        /// <summary>
+        /// Метод для добавления новой статьи
+        /// </summary>
+        /// <param name="reqest"></param>
+        /// <param name="userMod"></param>
+        /// <returns>StatusCode(200, newArticle)</returns>
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> CreateArticle(AddArticlesReqest reqest, User userMod)
+        {
+            //Проверяю пользователя на null
+            var user = await _user.GetUserById(userMod.Id);
+            if(user == null)
+                return StatusCode(400, $"Пользователь с Id {user} не найден!");
+
+            // Добавляю статью
+            var newArticle = _mapper.Map<AddArticlesReqest, Article>(reqest);
+            await _articl.CreateArticle(newArticle, user);
+
+            return StatusCode(200, newArticle);
+        }
+        /// <summary>
+        /// Находим статью по Id
+        /// </summary>
+        /// <param name="article"></param>
+        /// <returns>StatusCode(200, verifiableArticle)</returns>
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetsArticleById(Article article)
+        {
+            var verifiableArticle = await _articl.GetArticleById(article.Id);
+
+           return StatusCode(200, verifiableArticle);
+        }
+        //[HttpPatch]
+        //[Route("Id")]
+        //public async Task<IActionResult> UpdateArticle(
+        //    [FromRoute] Guid Id,
+        //    [FromBody] EditArticleRequest request
+        //    )
+        //{
+
+        //}
     }
 }
--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,7 +1,18 @@
 using AutoMapper;
+using BlogApp.Contracts.Models.Articles;
+using BlogApp.Contracts.Models.Comments;
+using BlogApp.Contracts.Models.Tegs;
+using BlogApp.Contracts.Models.Users;
+using BlogApp.Contracts.Validation.ArticlesValidators;
+using BlogApp.Contracts.Validation.CommentValidators;
+using BlogApp.Contracts.Validation.TegValidators;
+using BlogApp.Contracts.Validation.UserValidators;
 using BlogApp.Data.Context;
 using BlogApp.Data.Repositories;
+using BlogApp.Model;
 using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
@@ -23,10 +34,19 @@ builder.Services.AddSingleton<ItegRepository, TegRepository>();
 builder.Services.AddSingleton<ICommentRepository, CommentRepository>();
 builder.Services.AddSingleton<IArticlRepository, ArticleRepository>();
 
+// Регистрация валидиции
+builder.Services.AddScoped<IValidator<AddUserRequest>, AddUserRequestValidator>();
+builder.Services.AddScoped<IValidator<EditUserRequest>, EditUserRequestValidator>();
+builder.Services.AddScoped<IValidator<AddArticlesReqest>, AddArticlesRequestValidator>();
+builder.Services.AddScoped<IValidator<EditArticleRequest>, EditArticlesRequestValidator>();
+builder.Services.AddScoped<IValidator<AddCommentReqest>, AddCommentReqestValidator>();
+builder.Services.AddScoped<IValidator<EditCommentReqest>, EditCommentReqestValidator>();
+builder.Services.AddScoped<IValidator<AddTegRequest>, AddTegRequestValidator>();
+builder.Services.AddScoped<IValidator<EditTegRequest>, EditTegRequestValidator>();
+
 // Регистрирую строку подключения
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<BlogContext>(option => option.UseSqlServer(connectionString), ServiceLifetime.Singleton);
-
 
 
 builder.Services.AddRazorPages();
@@ -43,6 +63,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+//app.MapRazorPages();
 
 app.Run();
