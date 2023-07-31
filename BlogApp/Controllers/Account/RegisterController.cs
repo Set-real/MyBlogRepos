f6632479a6cd9e3d50c7bddc3f9bbeddff2d065a
Name <email@example.ru>
2023-07-31 16:11:08 +0300

    Mapping RegisterViewModel and LoginViewModel by User

--- BlogApp/Controllers/Account/RegisterController.cs
+++ BlogApp/Controllers/Account/RegisterController.cs
@@ -1,6 +1,56 @@
-﻿namespace BlogApp.Controllers.Account
+﻿using AutoMapper;
+using BlogApp.Contracts.Models.Users;
+using BlogApp.Data.Model.ViewModels;
+using BlogApp.Data.Repositories;
+using BlogApp.View.Pages;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers.Account
 {
-    public class RegisterController
+    public class RegisterController : Controller
     {
+        private readonly UserManager<UserRequest> _userManager;
+        private readonly SignInManager<UserRequest> _signInManager;
+        private readonly IMapper _mapper;
+        public RegisterController(UserManager<UserRequest> userManager, SignInManager<UserRequest> signInManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+        }
+
+        [Route("Register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("Home/Register");
+        }
+
+        [Route("Register")]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _mapper.Map<UserRequest>(model);
+
+                var result = await _userManager.CreateAsync(user, model.PasswordReg);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View("HomePages");
+        }
     }
 }
+
